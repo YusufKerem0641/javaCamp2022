1f2fdcd337b7a06855ebe1e2ecb488e892e8fa31
YusufKerem0641 <Yusuf0641421@outlook.com>
2022-09-26 22:14:14 +0300

    a

---
+++ odev1/miniProjectAsalSayi/src/miniProjectAsalSayi/Main.java
@@ -0,0 +1,34 @@
+package miniProjectAsalSayi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = -2;
+		int remainder = number % 2;
+		//System.out.println(remainder);
+		boolean isPrime = true;
+		
+		if(number == 1) {
+			System.out.println("Sayý asal deðildir ");
+			return ;
+		}
+		
+		if(number<2) {
+			System.out.println("Geçersiz sayý");
+		}
+		
+		for (int i = 2; i<number;i++) {
+			if(number % i == 0) {
+				isPrime = false;
+			}
+		}
+		
+		if (isPrime) {
+			System.out.println("Sayý asaldýr");
+		}else {
+			System.out.println("Sayý asal deðildir");
+		}
+
+	}
+
+}
